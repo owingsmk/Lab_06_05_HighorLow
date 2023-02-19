d4372ea6db6123522d5290038bd1517cadacb546
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 18:05:05 -0500

    First Commit

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,5 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HighorLow {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random rand = new Random();
+
+        int randomNumber =  rand.nextInt(10) + 1;
+
+        System.out.print("Enter your guess between (1-10): ");
+        Scanner num = new Scanner(System.in);
+        int playerGuess = Scanner.nextInt();
+        if (playerGuess == randomNumber){
+            System.out.println("Correct! You Win!");
+        } else if (randomNumber > playerGuess) {
+            System.out.println("Nope! The number is higher. Guess again.");
+        }else {
+            System.out.println("Nope! The number is lower. Guess again.");
+        }
+
+
     }
 }
