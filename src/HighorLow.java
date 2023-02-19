3e00adb67a65ea029b5b1dfb0c9df38a9178987a
Mark Owings <owingsmk@mail.uc.edu>
2023-02-19 18:12:32 -0500

    Final Commit

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,24 +1,38 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.Random;//Random number generator
+import java.util.Scanner;//Scanner for user input import
 
 public class HighorLow {
     public static void main(String[] args) {
 
         Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        int randomNumber =  rand.nextInt(10) + 1;
+        int randomNumber = rand.nextInt(10) + 1;
+        //System.out.println("Random number is " + randomNumber);
 
-        System.out.print("Enter your guess between (1-10): ");
-        Scanner num = new Scanner(System.in);
-        int playerGuess = Scanner.nextInt();
-        if (playerGuess == randomNumber){
-            System.out.println("Correct! You Win!");
-        } else if (randomNumber > playerGuess) {
-            System.out.println("Nope! The number is higher. Guess again.");
-        }else {
-            System.out.println("Nope! The number is lower. Guess again.");
+        int tryCount = 0;
+        while(true) {
+            System.out.println("Enter your guess (1-10):");
+
+            int playerGuess = scanner.nextInt();
+            tryCount++;
+
+            if (playerGuess == randomNumber) {
+                System.out.println("Correct! You Win!");
+                System.out.println("It took you " + tryCount + " tries");
+                break;
+            }
+            else if(randomNumber > playerGuess) {
+                System.out.println("Nope! The number is higher. Guess again.");
+            }
+            else {
+                System.out.println("Nope! The number is lower. Guess again.");
+            }
         }
 
+
+
+        scanner.close();
 
     }
 }
